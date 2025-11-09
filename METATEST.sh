da0aaf591b7028222f74afc5e29d3c2f08cbb46f
Hall, Andreas <andreas.hall@volvocars.com>
2025-11-09 10:53:19 +0100

    Adding testscript METATEST.sh

---
+++ METATEST.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OPT="$1"
+
+# set up source
+SRC=TEST/src
+TGT=TEST/tgt
+rm -rf "$SRC"/*
+mkdir -p "$SRC"/dir{1,2}/subdir{1,2}
+
+
+while read -r dir; do
+    fname="$(basename "$dir" | sed 's/dir/file/')"'.txt'
+    echo "Creating file: $dir/$fname"
+    touch "$dir/$fname"
+done < <(find $SRC -type d)
+
+
+METAOUT="$(PWD)/METAout.bin"
+rm  $METAOUT
+
+./rsync -a $OPT --meta-log="$METAOUT" --meta-top="MTOP" --meta-fmt='iTN' "$SRC"/ "$TGT"
